ea85d7699d334a4770ae42bd17cb1448f85333a6
pontesjpp <jpgp@cin.ufpe.br>
2025-02-26 00:34:24 -0300

    Update main.cpp

--- hashtables/main.cpp
+++ hashtables/main.cpp
@@ -6,7 +6,26 @@
 using namespace std;
 
 int main (int argc, char **argv) {
-    MyHash hashObj;
-    cout << "Paul => " << hashObj.Hash("Paul") << endl;
+    MyHash hashy;
+    string name;
+    hashy.addItem("Paul", "Locha");
+    hashy.addItem("Kim", "Iced Mocha");
+    hashy.addItem("Emma", "Strawberry Smoothy");
+    hashy.addItem("Annie", "Hot Chocolate");
+    hashy.addItem("Sarah", "Passion Tea");
+    hashy.addItem("Pepper", "Caramel Mocha");
+    hashy.addItem("Mike", "Chai Tea");
+    hashy.addItem("Steve", "Apple Cider");
+    hashy.addItem("Bill", "Root Beer");
+    hashy.addItem("Marie", "Skinny Latte");
+    hashy.addItem("Susan", "Water");
+
+    while (name!="exit")
+    {
+        cin>>name;
+        hashy.findDrink(name);
+    }
+    
+
     return 0;
 }
